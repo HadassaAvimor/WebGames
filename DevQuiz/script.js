0f3a03124b8a7c3472706b91e8e8799f27b75593
HadassaAvimor <h0533185978@gmail.com>
2023-09-22 04:47:55 +0300

    change main screen to switch assigment

--- DevQuiz/script.js
+++ DevQuiz/script.js
@@ -1,6 +1,8 @@
 
 // declaring variables here
-const startBtn = document.querySelector(".btn .start-btn");
+const startBtn = document.querySelector(".btn .start-btn"); 
+const pythonBtn = document.querySelector("#python-btn");
+const jsBtn = document.querySelector("#js-btn");
 const restartBtn = document.querySelector(".restart-btn");
 const mainScreen = document.querySelector(".main-screen");
 const question = document.querySelector(".question-panel h1");
@@ -17,7 +19,10 @@ const gameScoreBox = document.querySelector(".score-box");
 const totalScoreBox = document.querySelector(".total-score");
 const percentBox = document.querySelector(".percent");
 const remarkEmoji = document.querySelector(".remark-emoji");
-
+let python = false;
+let js = false;
+let questionsArray = [];
+let answersArray = [];
 //debugging variables and functions start
 const questionCountElement = document.querySelector(".question-count");
 const questionsAskedElement = document.querySelector(".questions-asked-array");
@@ -58,7 +63,7 @@ let score = 0,
 updateQuestionCountElement(questionCount);
 
 // questions
-const questionArray = [
+const jsQuestionArray = [
     "What does HTML stand for?",
     "Which of the following is not a JavaScript data type?",
     "What is the result of 2 + '2' in JavaScript?",
@@ -81,14 +86,62 @@ const questionArray = [
     "What is the difference between 'null' and 'undefined' in JavaScript?"
   ];
   
-  updateQuestionsArrayLengthElement(questionArray.length)
-let totalScore = (questionArray.length)*10;
-totalScoreBox.innerHTML = totalScore;
+  const pythonQuestionArray = [
+    "What does HTML stand for?",
+    "Which of the following is not a JavaScript data type?",
+    "What is the result of 2 + '2' in JavaScript?",
+    "What keyword is used to declare a variable in JavaScript?",
+    "Which built-in method adds one or more elements to the end of an array and returns the new length?",
+    "What does CSS stand for?",
+    "Which operator is used for equality comparison without type coercion in JavaScript?",
+    "What does API stand for?",
+    "What is the purpose of the 'use strict' directive in JavaScript?",
+    "Which function is used to parse a JSON string?",
+    "What is the main purpose of a constructor function in JavaScript?",
+    "Which method is used to remove the last element from an array in JavaScript?",
+    "What is the default behavior of the event.preventDefault() method?",
+    "Which keyword is used to declare a constant variable in JavaScript?",
+    "What is the purpose of a callback function in JavaScript?",
+    "Which global function is used to convert a string to an integer?",
+    "What does the acronym CRUD stand for in the context of databases?",
+    "What is the significance of the 'this' keyword in JavaScript?",
+    "Which method is used to schedule a function to run after a certain delay?",
+    "What is the difference between 'null' and 'undefined' in JavaScript?"
+  ];
+function calculateTotalScore(array){
+    updateQuestionsArrayLengthElement(array.length)
+    totalScoreBox.innerHTML = totalScore;
+    return (array.length)*10;
+}
 
+// let totalScore = calculateTotalScore(jsQuestionArray)
+let totalScore = 200
 
 //   options
 
-const mcqArray = [
+const mcqJSArray = [
+    ["High Text Markup Language", "Hyperlink and Text Markup Language", "Hyper Transfer Markup Language", "Hyper Text Markup Language"],
+    ["Boolean", "Alert", "Number", "String"],
+    ["Error", "4", "NaN", "22"],
+    ["int", "string", "variable", "var"],
+    ["addToEnd()", "concat()", "push()", "append()"],
+    ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+    ["!==", "===", "==", "="],
+    ["Advanced Programming Interface", "Automated Programming Interface", "Application Protocol Interface", "Application Programming Interface"],
+    ["Declares a variable", "Enforces stricter parsing and error handling", "Defines a function", "Includes an external script"],
+    ["JSON.stringify()", "JSON.serialize()", "JSON.parse()", "JSON.decode()"],
+    ["Memory management", "DOM manipulation", "Creating objects", "Error handling"],
+    ["shift()", "remove()", "pop()", "delete()"],
+    ["Cancels the default behavior of an element", "Prevents the event from bubbling up the DOM tree", "Stops the event propagation", "Prevents the event from capturing"],
+    ["const", "let", "constant", "var"],
+    ["Handling errors", "Passing a function as an argument to another function", "Executing an asynchronous operation", "Passing data to another domain"],
+    ["parseInt()", "toInteger()", "convertToInt()", "stringToInt()"],
+    ["Compile, Run, Upload, Debug", "Update, Read, Create, Delete", "Control, Repeat, Undo, Draw", "Copy, Resize, Underline, Delete"],
+    ["It refers to the previous function's scope", "It refers to the current function's scope", "It refers to the parent element's scope", "It refers to the global scope"],
+    ["setInterval()", "wait()", "delay()", "setTimeout()"],
+    ["'undefined' and 'null' can be used interchangeably", "'undefined' is an intentional absence of value, while 'null' indicates a variable that has been declared but not assigned a value", "'null' is an intentional absence of value, while 'undefined' indicates a variable that has been declared but not assigned a value", "'null' is the same as 'undefined'"]
+];
+const mcqPythonArray = [
     ["High Text Markup Language", "Hyperlink and Text Markup Language", "Hyper Transfer Markup Language", "Hyper Text Markup Language"],
     ["Boolean", "Alert", "Number", "String"],
     ["Error", "4", "NaN", "22"],
@@ -194,35 +247,45 @@ function toggleOptionPanelClickEvents()
 }
 
 // function will change questions and options
+function selectProgramingLanguage(){
+    if (python){
+        questionsArray =  pythonQuestionArray;
+        answersArray = mcqPythonArray;
+    }
+    else if (js){
+        questionsArray =  jsQuestionArray;
+        answersArray = mcqJSArray;
+    }
+}
 function changeQuestion() {
     optionA.classList.remove("right-option", "wrong-option");
     optionB.classList.remove("right-option", "wrong-option");
     optionC.classList.remove("right-option", "wrong-option");
     optionD.classList.remove("right-option", "wrong-option");
     toggleOptionPanelClickEvents();
-    questionIndex = Math.floor(Math.random()*questionArray.length);
+    questionIndex = Math.floor(Math.random()*jsQuestionArray.length);
     
-    if(questionsAsked.length < questionArray.length )
+    if(questionsAsked.length < questionsArray.length )
     {        
         while(questionsAsked.includes(questionIndex))
         {
-            questionIndex = Math.floor(Math.random()*questionArray.length);
+            questionIndex = Math.floor(Math.random()*questionsArray.length);
         }
         
         questionsAsked.push(questionIndex);
         updateQuestionsAskedElement(questionsAsked.length)
-        question.innerHTML = questionArray[questionIndex]; //change question text
+        question.innerHTML = questionsArray[questionIndex]; //change question text
         // change option text
-        optionA.innerHTML = mcqArray[questionIndex][0];
-        optionB.innerHTML = mcqArray[questionIndex][1];
-        optionC.innerHTML = mcqArray[questionIndex][2];
-        optionD.innerHTML = mcqArray[questionIndex][3];
+        optionA.innerHTML = answersArray[questionIndex][0];
+        optionB.innerHTML = answersArray[questionIndex][1];
+        optionC.innerHTML = answersArray[questionIndex][2];
+        optionD.innerHTML = answersArray[questionIndex][3];
 
         questionCount++;
         
         updateQuestionCountElement(questionCount);
         //this is what determines when the game is finished
-        if (questionCount > questionArray.length -1) showGameOverScreen(); 
+        if (questionCount > jsQuestionArray.length -1) showGameOverScreen(); 
         calculateTheFinalScore();        
     }else{        
         updateQuestionsAskedElement(questionsAsked.length);
@@ -278,11 +341,16 @@ function selectTheme() {
 
 
 // click start button on menu screen to perform
-startBtn.addEventListener("click",() => {
+pythonBtn.addEventListener("click",() => {
+    python = true;
     mainScreen.style.display = "none";
     toggleOptionPanelClickEvents();
 });
-
+jsBtn.addEventListener("click",() => {
+    js = true;
+    mainScreen.style.display = "none";
+    toggleOptionPanelClickEvents();
+});
 // click on restart button to restart game
 restartBtn.addEventListener("click", () => {
     // gameFunction();
